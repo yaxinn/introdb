d6ea7289f78af7424faf8d456b14994c66ac00dc
yaxinn <yaxin.t@berkeley.edu>
2015-01-28 07:51:58 +0000

    hw1 files update1: using readlines()

--- hw1/read.py
+++ hw1/read.py
@@ -15,38 +15,40 @@ fields_tokens = ["ebook_id", "token"]
 
 ### files to write
 # ebooks.csv
-ebook_csv = open('ebook.csv', 'wb')
+ebook_csv = open('ebook.csv', 'w')
 writer_eb = csv.DictWriter(ebook_csv, fieldnames=fields_ebook)
 writer_eb.writeheader()
 
 # tokens.csv
-tokens_csv = open('tokens.csv', 'wb')
+tokens_csv = open('tokens.csv', 'w')
 writer_tk = csv.DictWriter(tokens_csv, fieldnames=fields_tokens)
 writer_tk.writeheader()
 
 n_fg, c_fg = True, False
-for l in f:
-	if n_fg:
-		new = {"title": 'null', "author": 'null', "release_date": 'null', "ebook_id": 'null', "language": 'null', "body": []}		
-		n_fg = False
-	if fields[0] in l and not c_fg: new[fields_ebook[0]] = l.strip().split('Title:')[1].strip()
-	elif fields[1] in l and not c_fg: new[fields_ebook[1]] = l.strip().split(': ')[1].strip()
-	elif fields[2] in l and not c_fg:
-		t = re.split(r'[:\[\]\#]*', l.strip())
-		new[fields_ebook[2]] = t[1].strip()
-		new[fields_ebook[3]] = t[3]
-	elif fields[4] in l and not c_fg: new[fields_ebook[4]] = l.strip().split(': ')[1]
-	elif CONTENT_S in l: # start of body
-		c_fg = True
-	elif CONTENT_E in l: 
-		c_fg , n_fg = False, True
-		new['body'] = ''.join(new['body'])
-		writer_eb.writerow(new)
-		body = re.sub(r'[^\x00-\x7f]',r' ',new['body'])
-		body = re.compile(r'[\s0-9{}\n]+'.format(re.escape(punctuation))).split(body.strip())
-		[writer_tk.writerow({'ebook_id': new['ebook_id'], 'token': i.lower()}) for i in body if i.isalpha()]
-	elif c_fg: # inside the body
-		new['body'].append(l)
+while True:
+	chunck = f.readlines(2000)
+	for l in chunck:
+		if n_fg:
+			new = {"title": 'null', "author": 'null', "release_date": 'null', "ebook_id": 'null', "language": 'null', "body": []}		
+			n_fg = False
+		if fields[0] in l and not c_fg: new[fields_ebook[0]] = l.strip().split('Title:')[1].strip()
+		elif fields[1] in l and not c_fg: new[fields_ebook[1]] = l.strip().split(': ')[1].strip()
+		elif fields[2] in l and not c_fg:
+			t = re.split(r'[:\[\]\#]*', l.strip())
+			new[fields_ebook[2]] = t[1].strip()
+			new[fields_ebook[3]] = t[3]
+		elif fields[4] in l and not c_fg: new[fields_ebook[4]] = l.strip().split(': ')[1]
+		elif CONTENT_S in l: # start of body
+			c_fg = True
+		elif CONTENT_E in l: 
+			c_fg , n_fg = False, True
+			new['body'] = ''.join(new['body'])
+			writer_eb.writerow(new)
+			body = re.sub(r'[^\x00-\x7f]',r' ',new['body'])
+			body = re.compile(r'[\s0-9{}\n]+'.format(re.escape(punctuation))).split(body.strip())
+			[writer_tk.writerow({'ebook_id': new['ebook_id'], 'token': i.lower()}) for i in body if i.isalpha()]
+		elif c_fg: # inside the body
+			new['body'].append(l)
 
 # close all files
 f.close()
--- hw1/stat.py
+++ hw1/stat.py
@@ -12,24 +12,29 @@ fref.close()
 # read tokens
 file_name = str(sys.argv[1])
 f = open(file_name, 'r')
-tkCount_csv = open('token_counts.csv', 'wb')
+tkCount_csv = open('token_counts.csv', 'w')
 tkcnt_header = ['token', 'count']
 writer_cnt = csv.DictWriter(tkCount_csv, fieldnames=tkcnt_header)
 writer_cnt.writeheader()
+n_count = []
+while True:
+	chunck = f.readlines(2000)
+	if not chunck: break
+	for l in chunck:
+		l = l.strip().split(' ')
+		writer_cnt.writerow({'token': l[1], 'count': l[0]})
+		if l[1] in pop_name: n_count.append({'token': l[1], 'count': l[0]})
+tkCount_csv.close()
+f.close()
+
 
 # create a new name_count.csv file
-namecnt_csv = open('name_counts.csv', 'wb')
+namecnt_csv = open('name_counts.csv', 'w')
 namecnt_header = ['token', 'count']
 writer_namecnt = csv.DictWriter(namecnt_csv, fieldnames=namecnt_header)
 writer_namecnt.writeheader()
 
 # write tokens_count.csv and name_count.csv
-for l in f:
-	l = l.strip().split(' ')
-	writer_cnt.writerow({'token': l[1], 'count': l[0]})
-	if l[1] in pop_name:
-		writer_namecnt.writerow({'token': l[1], 'count': l[0]})
-
-f.close()
-tkCount_csv.close()
+for n in n_count:
+	writer_namecnt.writerow(n)
 namecnt_csv.close()
